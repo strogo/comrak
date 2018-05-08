ab6c94ef355ad9106f392d94ba88c657dad00fe2
Ashe Connor <ashe@kivikakk.ee>
2018-05-08 12:31:07 +1000

    update to 0.2.12

--- implementors/core/ops/deref/trait.DerefMut.js
+++ implementors/core/ops/deref/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["regex_syntax"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"regex_syntax/struct.Lit.html\" title=\"struct regex_syntax::Lit\">Lit</a>",];
+implementors["regex_syntax"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"regex_syntax/hir/literal/struct.Literal.html\" title=\"struct regex_syntax::hir::literal::Literal\">Literal</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
